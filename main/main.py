0b4c2f88be7674649a6365d631493da84ad5cf58
EinMatjes <112409210+EinMatjes@users.noreply.github.com>
2023-10-03 18:28:00 +0200

    change app ip to allow external connections

--- main/main.py
+++ main/main.py
@@ -53,5 +53,5 @@ class holding_register(Resource):
 api.add_resource(holding_register,"/register")
           
 if __name__ == "__main__":
-    app.run(debug=True)     
+    app.run(debug=True, host="0.0.0.0", port=5000)     
         

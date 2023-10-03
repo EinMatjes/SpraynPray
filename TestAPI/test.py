bad81b8d501d04a4229ae3416d91a0904c5637bf
EinMatjes <112409210+EinMatjes@users.noreply.github.com>
2023-10-03 17:57:52 +0200

    updated APITester target IP

--- TestAPI/test.py
+++ TestAPI/test.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://192.168.0.14:5000/"
 
 response = requests.put(BASE + "video/1" ,{"likes": 10, "name": "Mama", "views": 33})
 input()
--- main/APITester.py
+++ main/APITester.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://192.168.0.14:5000/"
 
 response = requests.put(BASE + "register" ,{"register_ID": 1, "register_Data": 3})
 

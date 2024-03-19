c707b1bd3def7c7ab11846dc1cdfbe4456f6f205
EinMatjes <112409210+EinMatjes@users.noreply.github.com>
2024-03-19 09:56:52 +0100

    Added some Comments

--- main/APITester.py
+++ main/APITester.py
@@ -1,8 +1,8 @@
 import requests
 
-BASE = "http://192.168.1.3:5000/"
+BASE = "http://192.168.1.3:5000/" # !!! This is the IP of the RasPi. Change the IP as nececarry for the hangar. Port stays the same.
 
-response = requests.put(BASE + "register" ,{"register_ID": 1005, "register_Data": 1})
-#response = requests.get(BASE + "register" ,{"register_ID": 1001})
+response = requests.put(BASE + "register" ,{"register_ID": 1005, "register_Data": 1}) #Example for put request. Used to send control messages/set control variables
+#response = requests.get(BASE + "register" ,{"register_ID": 1001}) #Example get request. Used to read control variables
 
-print(response.json())
+print(response.json())  #Prints the response of the request.
--- main/main.py
+++ main/main.py
@@ -6,7 +6,7 @@ from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
-client = ModbusClient(host="192.168.1.2", port=502, debug=True)
+client = ModbusClient(host="192.168.1.2", port=502, debug=True) #this is the IP of the Easy PLC. !!!Needs to be changed based on Beagle Router Settings / The static ip of the EASY PLC!!!
 
 #adress = 1001 
 #number = 1
@@ -30,10 +30,10 @@ class holding_register(Resource):
         if not client.open():                   #Checking if the PLC is connected
             abort("Unable to Connect to PLC")
         else:                                   
-            registers[args["register_ID"] ] = client.read_holding_registers(args["register_ID"], 1)  
-            print(f"Register Nr: {args['register_ID']} = {registers[args['register_ID']]}")
+            registers[args["register_ID"] ] = client.read_holding_registers(args["register_ID"], 1)  #Reading the Marker from the PLC
+            print(f"Register Nr: {args['register_ID']} = {registers[args['register_ID']]}")  
       
-        return registers[args["register_ID"]]
+        return registers[args["register_ID"]]   #return Marker Value
     
     
     def put(self):
@@ -44,18 +44,18 @@ class holding_register(Resource):
 
             #Start Cleaning Cycle Function condition check
             if args["register_ID"] == 1003:  #IF the Register ID of the PUT request is 33
-                registers[1] = client.read_holding_registers(1001,1)
-                if registers[1][0] != 1: #Checks MarkerWord 1. If MW1 != 1 means System not in Startpositi>
-                    return"Cannot Start Cleaning Cycle: System not in Startposition" #Aborts with error>
+                registers[1] = client.read_holding_registers(1001,1) # Reading Marker word 1 (system in Startposition)
+                if registers[1][0] != 1: #Checks MarkerWord 1. If MW1 != 1 means System not in Startposition
+                    return"Cannot Start Cleaning Cycle: System not in Startposition" #Aborts cause system not in Startposition
                     
 
             #Nozzle Cleaning Function condition check
             if args["register_ID"] == 1005: # IF the Register ID of the PUT request is 65
-                registers[1] = client.read_holding_registers(1001,1)
-                if registers[1][0] != 1: #Checks MarkerWord 1. If MW1 != 1 means System not in Startpositi>
-                    return "Cannot clean nozzle: System not in Startposition" #Aborts with error message
+                registers[1] = client.read_holding_registers(1001,1) # Reading Marker word 1 (system in Startposition)
+                if registers[1][0] != 1: #Checks MarkerWord 1. If MW1 != 1 means System not in Startposition
+                    return "Cannot clean nozzle: System not in Startposition" #Aborts cause system not in Startposition
 
-            client.write_single_register(args["register_ID"], args["register_Data"]) # If the System is >
+            client.write_single_register(args["register_ID"], args["register_Data"]) # If system in Startposition. Writing the desired Markerword to desiered Data ... (data should normally always be int = 1 )
 
         return "Succesfully updated"
 

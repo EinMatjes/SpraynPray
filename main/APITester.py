c65868ddd931a2bd97b11246120cd4b5a18ba6e3
EinMatjes <112409210+EinMatjes@users.noreply.github.com>
2024-03-19 09:58:58 +0100

    some more Comments

--- main/APITester.py
+++ main/APITester.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE = "http://192.168.1.3:5000/" # !!! This is the IP of the RasPi. Change the IP as nececarry for the hangar. Port stays the same.
+BASE = "http://192.168.1.3:5000/" # !!! This is the IP of the RasPi. Change the IP as nececarry for the hangars DHCP IP. Port stays the same (5000).
 
 response = requests.put(BASE + "register" ,{"register_ID": 1005, "register_Data": 1}) #Example for put request. Used to send control messages/set control variables
 #response = requests.get(BASE + "register" ,{"register_ID": 1001}) #Example get request. Used to read control variables
